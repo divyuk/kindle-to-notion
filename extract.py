8b6c337bd23576aba4344717a68ad9bf42839474
divyuk <div14kes@gmail.com>
2022-12-10 21:36:05 +0530

    The extraction of data with duplicate removal

--- extract.py
+++ extract.py
@@ -1,15 +1,40 @@
 import re
+import json
+
 myfile = "test.txt"
-lines_list = []
+flag = True
+extracted_data = []
+my_dict=dict()
+highlights = []
 regex_exp = r'(?P<Book>.+) \((?P<Author>.+)\)\r*\n- Your (?P<type>Highlight)(?:.+)\r*\n\r*\n(?P<content>.+)'
-with open(myfile , 'r', encoding="utf8") as file:
+with open(myfile , 'r', encoding="utf-8-sig") as file:
     try:
         f = file.read()
+        f = re.sub("\uFEFF", "", f)
     except Exception as e:
         raise e 
-
-    
+  
     iterator = re.finditer(regex_exp,f)
-    # final_string = "".join(matched_string)
     for i in iterator:
-        print(i.group('Book'))
+        for j  in extracted_data:
+            if j["title"] == i.group('Book') : 
+                j["highlights"].append(i.group("content"))
+                flag = False
+                break
+        if flag:
+            my_dict["title"] = i.group('Book')
+            my_dict["author"] = i.group("Author")
+            highlights.append(i.group("content"))
+            my_dict["highlights"] = highlights
+            extracted_data.append(my_dict)
+        my_dict={}
+        highlights=[]
+        flag = True
+    d = []
+    for k in extracted_data:
+       k["highlights"]= list(set(k["highlights"]))
+        
+
+print(extracted_data)
+with open('data.json', 'w',  encoding="utf-8") as f:
+    json.dump(extracted_data, f, ensure_ascii = False) 
